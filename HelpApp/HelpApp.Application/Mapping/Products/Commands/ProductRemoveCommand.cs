2111ce248e806cc42c9ae067cce942ff8cf5984d
PedroHBachiega <pedrobachiega22@gmail.com>
2025-05-19 12:44:45 -0300

    Comand e Handlers

---
+++ HelpApp/HelpApp.Application/Mapping/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,14 @@
+﻿using HelpApp.Domain.Entities;
+using MediatR;
+
+namespace HelpApp.Application.Mapping.Products.Commands
+{
+    public class ProductRemoveCommand : IRequest<Product>
+    {
+        public int id { get; set; }
+        public ProductRemoveCommand(int id)
+        {
+            id = id;
+        }
+    }
+}
