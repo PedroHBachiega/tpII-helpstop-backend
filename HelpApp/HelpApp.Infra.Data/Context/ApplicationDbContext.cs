9d07acb5b51ecbc5841c9e1794c12fb19a9eb400
PedroHBachiega <pedrobachiega22@gmail.com>
2025-06-02 12:53:32 -0300

    Aula 0206 Identity, criação da migration

--- HelpApp/HelpApp.Infra.Data/Context/ApplicationDbContext.cs
+++ HelpApp/HelpApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using HelpApp.Domain.Entities;
+using HelpApp.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Category> Categories { get; set; }
